fa06ba1fe4d58994161267fd582b07b8f6b74188
Nick Chen <a0958577500@gmail.com>
2024-06-12 11:07:11 +0800

    chore: upgrade lib & eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,8 @@
 module.exports = {
   extends: ['@nick12003/eslint-config-nick/typescript'],
   ignorePatterns: ['dist/'],
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: ['./tsconfig.json'],
-      },
-    },
-  },
-  rules: {
-    '@typescript-eslint/no-unsafe-assignment': 0,
-    '@typescript-eslint/no-unsafe-member-access': 0,
-    '@typescript-eslint/no-unsafe-argument': 0,
-    '@typescript-eslint/no-unsafe-return': 0,
-    '@typescript-eslint/no-unsafe-call': 0,
-    '@typescript-eslint/no-explicit-any': 0,
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
   },
 };
